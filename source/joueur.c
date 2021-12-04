2c2d5bf71ad4f08bfe0b499832a3503514cc4b4d
Kuuroojinn <94182798+Kuuroojinn@users.noreply.github.com>
2021-12-04 14:21:40 +0100

    Update joueur.c
    J'ai modifié ce que j'avais déjà commencé hier, et du coup la fonction deplacement à l'air de focntionner, 
    J'ai aussi modifié un peu map.c pour des tests, si jamais vous voulez voir allez regarder dans le dossier test

--- source/joueur.c
+++ source/joueur.c
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <assert.h>
+#include <stdio.h>
 
 
 
@@ -13,3 +14,36 @@ struct joueur
 	// pour les combats, à chager plus tard
 	int pv = 1000;
 };
+
+//Modifie la position du joueur en fonction de l'entrée au clavier
+void deplacement(struct joueur* j,int c) {
+	if (c == KEY_UP) {
+		j->pos_lin -= 1;
+	} else if (c == KEY_DOWN) {
+		j->pos_lin += 1;
+	} else if (c == KEY_RIGHT) {
+		j->pos_col += 1;
+	} else if (c == KEY_LEFT) {
+		j->pos_col -= 1;
+	}
+}
+
+/* Pour tester la fontction, j'ai réutillisé ce que j'avais écris dans la fonction test/map.c
+La fonction à l'air de marcher, vous pouvez essayer de la tester sur vos pc au cas ou */
+int main() {
+	initscr();
+	keypad(stdscr, TRUE); // a ne pas oublier pour utiliser KEY_UP, etc
+	noecho();
+	struct joueur j1;
+	j1.pos_lin = -10;
+	j1.pos_col = 10;
+	int c = -1; //Bien penser à le laisser en int pour pouvoir être comparé avec KEY_UP, etc
+	while (c != 'q') { //Marche aussi, il lit juste 'q' en tant que int pour y comparer
+		c = getch();
+		deplacement(&j1, c);
+		printw("pos_x = %d\n",j1.pos_lin);
+		printw("pos_y = %d\n",j1.pos_col);
+	}
+	endwin();
+	return 0;
+}
