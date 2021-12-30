b16ce3dd91f1106266ea1487f1a0f02ddd4f14a5
TheBord3rLine <94250414+TheBord3rLine@users.noreply.github.com>
2021-12-30 02:10:13 +0100

    Couloirs verticaux
    Même chose que pour les couloirs horizontaux

--- src/salles.c
+++ src/salles.c
@@ -12,6 +12,8 @@ const int LONG_C_V = 4; //longueur d'un couloir vertical
 
 const int ORI_C_H_A = 29; //origine des couloirs horizontaux (colonne)
 int ORI_C_H_O = 4; //origine des couloirs horizontaux (ligne)
+const int ORI_C_V_A = 16; //origine des couloirs verticaux (colonne)
+int ORI_C_V_O = 9; //origine des couloirs verticaux (ligne)
 
 /* entoure la salle de MURs et la remplit de VIDE */
 void initialise_salle(struct salle* s)
@@ -76,5 +78,28 @@ void ajoute_couloir_h(int map[MAP_LIN][MAP_COL])
 			}
 		}
 		ORI_C_H_O = ORI_C_H_O + LONG_C_V + SAL_LIN - 2;
+	}
+}
+
+/* ajoute les couloirs verticaux */
+void ajoute_couloir_v(int map[MAP_LIN][MAP_COL])
+{	
+	for (int j = 0; j < 2; j = j + 1) //2 rangées de couloirs
+	{
+		for (int ligne = 0; ligne < LONG_C_V; ligne = ligne + 1) 
+		//le couloir prend 4 lignes
+		{
+			for (int i = ORI_C_V_A; i < MAP_COL - 4; i = i + LONG_C_H + SAL_COL)  
+			//on ne doit pas dépasser la map (avec 4 la largeur du couloir)
+			{
+				map[ORI_C_V_O][i]= MUR;
+				map[ORI_C_V_O][i + 1] = VIDE;
+				map[ORI_C_V_O][i + 2] = VIDE;
+				map[ORI_C_V_O][i + 3] = MUR;
+			}
+			ORI_C_V_O = ORI_C_V_O + 1;
+		}
+		ORI_C_V_O = ORI_C_V_O + LONG_C_V + SAL_LIN - 6; 
+		//"-6" car il faut retirer les 4 rajouter dans la boucle précédente
 	}
 }
