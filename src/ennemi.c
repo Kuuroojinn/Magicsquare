ee69b60e2c519cd1ae8e12688eb2df5e72ce3e3d
Kuuroojinn <94182798+Kuuroojinn@users.noreply.github.com>
2022-01-08 14:22:32 +0100

    Add files via upload

--- src/ennemi.c
+++ src/ennemi.c
@@ -23,41 +23,45 @@ struct ennemi e1 = {1};
 bool combat (struct joueur* j, struct ennemi* e){
 	char entree;
 	int action;
-	printw("Etes-vous sur de vouloir combattre ? Oui[o] ou Non [n]");
-	scanw("%s",&entree);
-	if (entree == 'n'){
-		return false;   //ON considère donc que le joueur a perdu le combat
-	}
-	
 	int pv_max = j->pv;
 	int pv_ennemi = e->pv;
-	while ((j->pv > 0) && (e->pv >0)){
-		printw("Vous : %d | Ennemi : %d",j->pv,e->pv);
-		printw("Que voulez vous faire : attaquer[a] ou defendre[d] ? ");
-		scanw("%s",&entree);
-		if (entree == 'a') {
-			action = rand()&1; //Genere un nombre aléatoire entre 0 et 1
-			if (action == 0) {
-				printw("L'adversaire attaque aussi\n");
-				e->pv = e->pv - j->atk;
-				j->pv = j->pv - e->atk;
-			} else {
-				printw("L'adversaire se défends\n");
-				e->pv = e->pv - (j->atk / 2);
-			}
-		} else if (entree == 'd') {
-			action = rand()&1;
-			if (action == 0) {
-				printw("L'aversaire se défends\n");
-				j->pv = j->pv - (e->pv / 2);
-			} else {
-				printw("L'adversaire se défends aussi\n");
+	printw("Etes-vous sur de vouloir combattre ? Oui[o] ou Non [n]");
+	scanw("%s",&entree); 
+	if (entree == 'o'){
+	
+		while ((j->pv > 0) && (e->pv >0)){
+			printw("Vous : %d | Ennemi : %d",j->pv,e->pv);
+			printw("Que voulez vous faire : attaquer[a] ou defendre[d] ? ");
+			scanw("%s",&entree);
+			if (entree == 'a') {
+				action = rand()&1; //Genere un nombre aléatoire entre 0 et 1
+				if (action == 0) {
+					printw("L'adversaire attaque aussi\n");
+					e->pv = e->pv - j->atk;
+					j->pv = j->pv - e->atk;
+				} else {
+					printw("L'adversaire se défends\n");
+					e->pv = e->pv - (j->atk / 2);
+				}
+			} else if (entree == 'd') {
+				action = rand()&1;
+				if (action == 0) {
+					printw("L'aversaire se défends\n");
+					j->pv = j->pv - (e->pv / 2);
+				} else {
+					printw("L'adversaire se défends aussi\n");
+				}
 			}
 		}
+	} 
+	else 
+	{
+		return false;
 	}
 	if (e->pv <= 0) {
 		j->atk +=1;
-		j->pv = pv_max;         //Remets les pv du joueur a ceux d'avant le combat
+		//j->pv = pv_max;         //Remets les pv du joueur a ceux d'avant le combat
+		j->pv +=5; //redonne 5 pv a la fin d'un combat
 		e->pv = pv_ennemi + 5; //la vie des ennemis augmente au fur et a mesure pour rajouter un peu de difficulté (peut être modifié)
 		e->atk = e->atk + rand()&1; //Augmente de 1 ou de 0 l'attaque de l'adversaire pour corser le jeu
 		return true;
